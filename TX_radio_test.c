52932863e921283fcdeb6b9db0a38c71a82d3419
Arthur Vie <arthur.vie@npl.co.uk>
2022-02-14 21:16:54 +0000

    A cursed bug. The default recipe for C compilation is compile only, no linking

--- TX_radio_test.c
+++ TX_radio_test.c
@@ -1,12 +1,27 @@
+#define _AVR_ATmega328p_
+#define F_CPU 16000000UL
+
 #include <stdint.h>
 #include <avr/io.h>
+#include <util/delay.h>
+
+//#include "Arthurs_libraries/AV_USART.h"
+
+char * test_string = "Hello world!";
 
 int main(void)
 {
+    DDRB = 0xFF;
+    //USART_init(F_CPU, 9600);
+
     while (1)
     {
-
+        //USART_transmit_multi_bytes((uint8_t *)test_string, 12);
+        PORTB = 0xFF;
+        _delay_ms(500);
+        PORTB = 0x00;
+        _delay_ms(500);
     }
-    
+
     return 0;
 }
--- makefile
+++ makefile
@@ -15,9 +15,13 @@ prog.hex: TX_radio_test.o
 	$(OBJCOPY) -O ihex $< $@
 
 TX_radio_test.o: TX_radio_test.c
+	$(CC) $(CFLAGS) -o $@ $^
+
+fuses:
+	$(PROGRAMMER) $(PROG_FLAGS) -P /dev/ttyUSB0 -U lfuse:w:0xFF:m -U hfuse:w:0xDF:m -U efuse:w:0xFD:m
 
 flash:
-	$(PROGRAMMER) $(PROG_FLAGS) -P /dev/tty??? -U flash:w:prog.hex
+	$(PROGRAMMER) $(PROG_FLAGS) -P /dev/ttyUSB0 -U flash:w:prog.hex
 
 clean:
 	rm *.o *.hex
