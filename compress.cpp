ff54fa4dc4f709f54b61090f0eced17ff76353da
Arthur Vie <arthur.vie@npl.co.uk>
2022-03-15 15:16:01 +0000

    continuing to implement methods and refine data structures.

--- compress.cpp
+++ compress.cpp
@@ -1,7 +1,7 @@
 #include <stdint.h>
 #include "compress.h"
 
-void compressor::pack_quaternions(float r, float i, float j, float k)
+void Compressor::pack_quaternions(float r, float i, float j, float k)
 {
     _packed_data.quaternion_r = (int32_t)(r * 100);
     _packed_data.quaternion_i = (int32_t)(i * 100);
@@ -9,8 +9,59 @@ void compressor::pack_quaternions(float r, float i, float j, float k)
     _packed_data.quaternion_k = (int32_t)(k * 100);
 }
 
-void compressor::pack_BME280_temp(float temperature)
+void Compressor::pack_BME280_temp(float temperature)
 {
     temperature = temperature > 60 ? 60 : temperature;
     temperature = temperature < -5.0 ? -5.0 : temperature; 
+    _packed_data.BME280_temp = (uint8_t)temperature;
+}
+
+void Compressor::pack_altitude(float altitude)
+{
+
+}
+
+void Compressor::pack_humidity(float humidity)
+{
+
+}
+
+void Compressor::pack_thermocouple_1(float temperature)
+{
+
+}
+
+void Compressor::pack_thermocouple_2(float temperature)
+{
+
+}
+
+void Compressor::pack_thermocouple_3(float temperature)
+{
+
+}
+
+void Compressor::pack_thermocouple_4(float temperature)
+{
+
+}
+
+void Compressor::pack_battery_voltage(float voltage)
+{
+
+}
+
+void Compressor::pack_GPS_long(float angle)
+{
+
+}
+
+void Compressor::pack_GPS_lat(float angle)
+{
+    
+}
+
+void Compressor::pack_GPS_heading(float angle)
+{
+    
 }
--- compress.h
+++ compress.h
@@ -3,15 +3,18 @@
 
 #include <stdint.h>
 
-class compressor
+class Compressor
 {
     public:
 
+    Compressor();
+
+    void set_status(uint8_t status);
     void pack_quaternions(float r, float i, float j, float k);
     void pack_BME280_temp(float temperature);
     void pack_altitude(float altitude);
     void pack_humidity(float humidity);
-    void pack_engine_bay_thermocouple(float temperature);
+    void pack_thermocouple_1(float temperature);
     void pack_thermocouple_2(float temperature);
     void pack_thermocouple_3(float temperature);
     void pack_thermocouple_4(float temperature);
@@ -20,48 +23,35 @@ class compressor
     void pack_GPS_lat(float angle);
     void pack_GPS_heading(float angle);
 
+    uint8_t * return_packet_buffer();
+
     private:
 
-    struct
+    union
     {
-        uint8_t quaternion_r:7;
-        uint8_t quaternion_i:7;
-        uint8_t quaternion_j:7;
-        uint8_t quaternion_k:7;
-        uint8_t BMP280_temp:7;
-        uint16_t altitude:12;
-        uint8_t humidity:7;
-        uint8_t engine_bay_thermocouple:8;
-        uint8_t thermocouple_1:7;
-        uint8_t thermocouple_2:7;
-        uint8_t thermocouple_3:7;
-        uint8_t battery_voltage:3;
-        uint8_t GPS_long; // needs sizing!!
-        uint8_t GPS_lat; // needs sizing!!
-        uint8_t GPS_heading; // needs sizing!!
-        uint8_t:3;
+        uint8_t _packet_buffer[15];
 
-    } _packed_data;
+        struct
+        {
+            uint8_t status:3;
+            uint8_t quaternion_r:7; // 0.01 resolution
+            uint8_t quaternion_i:7; // 0.01 resolution
+            uint8_t quaternion_j:7; // 0.01 resolution
+            uint8_t quaternion_k:7; // 0.01 resolution
+            uint8_t BME280_temp:7; // 0.5 degree C resolution between -5 and +60
+            uint16_t altitude:12; // 1 m resolution. 4096 m range 
+            uint8_t humidity:7; // 0.78% resolution
+            uint8_t thermocouple_1:7; // range tbc
+            uint8_t thermocouple_2:7; // range tbc
+            uint8_t thermocouple_3:7; // range tbc
+            uint8_t thermocouple_4:7; // range tbc
+            uint8_t battery_voltage:3; // 0.1125 V resolution between 3.3 V and 4.2 V
+            uint16_t GPS_pos_x:12; // 1 m resolution over 4 km on a projected cartesian grid
+            uint16_t GPS_pos_y:12; // 1 m resolution over 4 km on a projected cartesian grid
+            uint8_t GPS_heading:8; // 1.4 degree resolution
+            // timebase??
+        } _packed_data;
+    };
 };
-
-struct
-{
-    uint8_t quaternion_x:7;
-    uint8_t quaternion_i:7;
-    uint8_t quaternion_j:7;
-    uint8_t quaternion_k:7;
-    uint8_t BMP280_temp:7;
-    uint16_t altitude:12;
-    uint8_t humidity:7;
-    uint8_t engine_bay_thermocouple:8;
-    uint8_t thermocouple_1:7;
-    uint8_t thermocouple_2:7;
-    uint8_t thermocouple_3:7;
-    uint8_t battery_voltage:3;
-    uint8_t GPS_long; // needs sizing!!
-    uint8_t GPS_lat; // needs sizing!!
-    uint8_t GPS_heading; // needs sizing!!
-
-} packed_data;
 
 #endif
